a86517e384e173f3602dd24138f35008d3635196
Matthieu Bietry <matthieu@thechatbotfactory.com>
2021-01-16 10:29:39 +0100

    Updates

---
+++ precache-manifest.7d1a77d38dab9b4869a65a5d2af7bb7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbb8fdd4b9d3455bc184e757670b6f28",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a8a61ec3ece1b67705ec",
+    "url": "./static/css/main.b427710c.chunk.css"
+  },
+  {
+    "revision": "12282d455d8ebdd92c4d",
+    "url": "./static/js/2.adc089c3.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "./static/js/2.adc089c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8a61ec3ece1b67705ec",
+    "url": "./static/js/main.08e2bee9.chunk.js"
+  },
+  {
+    "revision": "dd7b323c43b450bc42d7",
+    "url": "./static/js/runtime-main.237a64a8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4e9ac9f65557f2eeae7ccde9e6217d60.js"
+  "./precache-manifest.7d1a77d38dab9b4869a65a5d2af7bb7e.js"
 );
 
 self.addEventListener('message', (event) => {
